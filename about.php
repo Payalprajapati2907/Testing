dbdb0de11f320de3566ea2a110a9c4ff3f456207
Payalprajapati2907 <42430737+Payalprajapati2907@users.noreply.github.com>
2021-09-14 14:27:06 +0530

    It is about page file
    about page content

---
+++ about.php
@@ -0,0 +1,90 @@
+<html>
+<head>
+<title>Ahmedabad Municipal Transport Services</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />	
+<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<!---start-header---->
+			<div class="header">
+				<div class="wrap">
+				<div class="logo">
+					<a href="index.php"><img src="images/logo.png" title="logo" /></a>
+				</div>
+				<div class="logo_name">
+				<h1>Ahmedabad Municipal Transport Services</h1>
+				</div></div>
+				<div class="top-nav">
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li class="active"><a href="about.php">About</a></li>
+						<li><a href="search_form.php">Search</a></li>
+						<li><a href="services.php">Services</a></li>
+						<li><a href="workshop.php">Work Shop</a></li>
+						<li><a href="contact.php">Contact</a></li>
+					</ul>
+				</div>
+				<div class="clear"> </div>
+			</div>
+		</div>
+			<!---End-header---->
+		<!---start-content---->
+		<div class="content">
+			<div class="wrap">
+				<div class="about-us">
+					 <div class="about-header">
+					    <h3>About us</h3>
+					 </div>
+					<div class="about-info">
+
+<center><img src="images/about.jpg"></center><br>
+ <p>Amdavad is well known among ancient cities of India. Ahmed Shah, ruler of Gujarat in those days established this city in 1411, hence it was named as Ahmedabad, and with the passage of time it became famous as Amdavad. There is a saying about the birth of this city: 
+</p>
+<p>“Jab kutte par Sassa aayaa, Tab Badshah ne shahar basaayaa”.
+</p>
+<p>(When the rabbit attacked the dog, the Sultan populated the city).
+</p>
+<p align="justify">This saying indicates something rather unique, as compared to the general events. The sultan must have visualized something special on the land of this place, therefore he selected it for establishing a city. Many peculiarities in civilization, sculpture, architecture, trade & commerce and industrial development are class apart even today. Economy and practical dexterity have grown with the life of a man from Amdavad. There is an unique personality in the resident of Amdavad, staying in remote foreign countries or in various other states of India; and they can be marked different. The father of Nation Mahatma Gandhi had started the movement of freedom, keeping in view Independence of India, and its functional centre was Amdavad. Ashrams st Kochrab and at Sabarmati were the main centre of the Non-co-operation movement Strongly attached to truth and non-violence, Respected BAPU had published two periodicals – NAVJIVAN (Gujarati) and YOUNG-INDIA (English) and that had spread the values throughout India. Gujarat Vidyapith has been founded here in order to establish the education system according to Indian culture and to abandon the British education, that nurtured the mentality of slaves. Great personalities such as Vinoba Bhave and Kaka Kalelkar had stayed at Sabarmati Ashram and Vidhapith here in Amdavad, for in many years. Jahangir, the moghul ruler at Delhi, had situated at Shahibaug near the bank of Sabarmati river, reigned as the commander of Gujarat for some time. The Sardar-Memorial, old secretariat building has been constructed in those days. Sir Thomas Row, a Dutch representative, who wanted the trade licence from moghul emperor (just like the British did), also resided here at PHUVARA in a big mansion named VALANDA – HAVELI (Residence of the Dutch) but that building is ruined at present. Sardar Vallabhbhai Patel, the Iron-man of India, was practising his career as lawyer and latter he served in the municipality, too. Amdavad was famous for textile, brocade and indigo industries in old days. The craftsmanslip was world famous, and brocade was highly demanded in foreign countries. Rai Bahadur Ranchhodlal Chhotalal had started the first Textile-mill at Amdavad in 1859, thereafter the textile industry developed to such an extent that Amdavad was known as Manchester of India. Small industries producing equipments and spare parts used in textiles also flourished here, and the industrial development speeded up, as a result the textile research institute (ATIRA) was founded at Amdavad. </p>
+				</div>
+					</br>
+					<center>
+					<a class="button" href="about_more.php">More</a></center>
+				</div>
+			</div>
+			     
+			<div class="clear"> </div>
+		<!---start-footer---->
+<div class="footer">
+			<div class="wrap">
+			<div class="footer-grids">
+			
+				<div class="footer-grid">
+					  <ul><li><a href="registration_form.php">registration Form</a></li>
+					  </ul>
+				</div>
+				<div class="footer-grid">
+					  <ul><li><a href="complain_form.php">complain Form</a></li>
+					  </ul>
+				</div>
+				<div class="footer-grid" >
+					  <ul><li><a href="event_form.php">Hire Bus Form</a></li>
+					  </ul>
+				</div>
+				<div class="footer-grid" >
+					  <ul><li><a href="advertisement_form.php">ADVERTISEMENT FORM</a></li>
+					  </ul>
+				</div>
+				<div class="clear"> </div>
+			</div>
+			</div>
+		</div>
+		<!---End-footer---->
+		<div class="clear"> </div>
+		<div class="copy-right">
+			<p>Design by <a href="">Jatin Fefar & Uttam Gadhesariya</a></p>
+		</div>
+	</body>
+</html>
+
